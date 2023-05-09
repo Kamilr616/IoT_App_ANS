9d1d88774e561f6fe30550adfc9e384306d5ccac
HOLY270 <110671590+HOLY270@users.noreply.github.com>
2023-05-09 20:14:57 +0200

    SettingsActivity

---
+++ app/src/main/java/com/example/iot_app_ans/StatisticActivity.java
@@ -0,0 +1,14 @@
+package com.example.iot_app_ans;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StatisticActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statystki1);
+    }
+}
