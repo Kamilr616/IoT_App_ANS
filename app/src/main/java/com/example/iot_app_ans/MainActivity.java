efd1dc3a57e99173c1300523b937dd67b760046d
HOLY270 <110671590+HOLY270@users.noreply.github.com>
2023-05-09 21:29:45 +0200

    Dodano przycisk settings

--- app/src/main/java/com/example/iot_app_ans/MainActivity.java
+++ app/src/main/java/com/example/iot_app_ans/MainActivity.java
@@ -1,16 +1,31 @@
 package com.example.iot_app_ans;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button buttonSettings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home1);
 
+        buttonSettings = findViewById(R.id. Settings);
+
+        buttonSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NewActivity();
+            }
+        });
+
+    }
+
+    private void NewActivity() {
+        setContentView(R.layout.activity_settings);
     }
 
 }
