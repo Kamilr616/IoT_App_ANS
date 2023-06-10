19fb13875ff8a361c3dd0262dbf3dfd4a64162d4
HOLY270 <110671590+HOLY270@users.noreply.github.com>
2023-06-10 11:35:43 +0200

    Slider przy MainActivity

--- app/src/main/java/com/example/iot_app_ans/MainActivity.java
+++ app/src/main/java/com/example/iot_app_ans/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.iot_app_ans;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.content.Intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    float x1, x2, y1, y2;
     Button buttonSettings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public boolean onTouchEvent(MotionEvent touchevent){
+        switch (touchevent.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                x1 = touchevent.getX();
+                y1 = touchevent.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = touchevent.getX();
+                y2 = touchevent.getY();
+                if(x2 < x1){
+                    Intent i = new Intent(MainActivity.this, MainActivity2.class);
+                    startActivity(i);
+                }
+                break;
+        }
+        return false;
     }
 
     private void NewActivity() {
